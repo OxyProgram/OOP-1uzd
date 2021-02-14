914c0f3c9ba9875179bf8df4df177b6fde74e5fd
Karolis Kučinskas <karolis.kucinskas@mif.stud.vu.lt>
2021-02-14 14:03:54 +0200

    v0.2

--- 1uzd.cpp
+++ 1uzd.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+void male(string name);
+void female(string name);
+
 //Karolis Kucinskas ISI 1 gr. 1 pgr. v0.1
 
 int main() {
@@ -17,8 +20,34 @@ int main() {
         return 0;
     }
 
+    cout << "Pasirinkite savo lytį (Įveskite atitinkamą skaičių): " << endl;
+    cout << "1. Vyras \n2. Moteris   " << flush;
+    char s;
+    try {
+        cin >> s;
+    } catch (char* e) {
+        cout << "Ivyko klaida!" << endl;
+        return 0;
+    }
+
     cout << endl;
 
+    switch(s) {
+        case '1':
+            male(name);
+            break;
+            
+        case '2':
+            female(name);
+            break;
+        default:
+            cout << "Įvyko klaida" << endl;
+            return 0;
+    }
+    return 0;
+}
+
+void male(string name) {
     string pirma = string(11 + name.size() + 2, '*');
     cout << pirma << endl;
 
@@ -33,6 +62,21 @@ int main() {
 
     string penkta = pirma;
     cout << penkta << endl;
+}
 
-    return 0;
+void female(string name) {
+    string pirma = string(10 + name.size() + 2, '*');
+    cout << pirma << endl;
+
+    string antra = "*" + string(pirma.size()-2, ' ') + "*";
+    cout << antra << endl;
+
+    string trecia = "* Sveika, " + name + " *";
+    cout << trecia << endl;
+
+    string ketvirta = antra;
+    cout << ketvirta << endl;
+
+    string penkta = pirma;
+    cout << penkta << endl;
 }
